e96fb2d77c7ebef739e4300ba5989f5b20a80972
eric-klimowich <eklimowich@hotmail.com>
2019-08-15 13:58:17 -0400

    Timed animations using callbacks instead of delay()

--- js/script.js
+++ js/script.js
@@ -81,7 +81,22 @@ $(function() {
   // $(".green-box").delay(1000).fadeTo(1000, 0.5)
   // $(".blue-box").delay(2000).fadeTo(1000, 0.8)
 
-  $(".red-box").fadeTo(1000, 0.2)
-  $(".green-box").delay(1000).fadeTo(1000, 0.5)
-  $(".blue-box").delay(2000).fadeTo(1000, 0.8).fadeOut().delay(500).fadeIn()
+  // $(".red-box").fadeTo(1000, 0.2)
+  // $(".green-box").delay(1000).fadeTo(1000, 0.5)
+  // $(".blue-box").delay(2000).fadeTo(1000, 0.8).fadeOut().delay(500).fadeIn()
+
+
+  // 15. Timing Animations Using Callback Functions
+  // $(".red-box").fadeTo(1000, 0, function() {
+  //   alert("This is the callback: Animation Finished!")
+  // })
+
+  $(".red-box").fadeTo(1000, 0, function() {
+    $(".green-box").fadeTo(1000, 0, function() {
+      $(".blue-box").fadeTo(1000, 0)
+    })
+  })
+  // A callback function can be used as the final parameter in any of the jQuery animation functions we have used.
+
+  // Also, callbacks are used when you want your animation to finish completely before the next action. If you wanted the second box to start fading a half a second after the first started, you would have to use the delay function.
 });
