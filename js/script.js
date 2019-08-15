f0710e2895f7d652a638cac5d7ffcdf1d2b8dbb8
eric-klimowich <eklimowich@hotmail.com>
2019-08-15 14:44:36 -0400

    Used CSS selectors to change the styles of elements on the page using jQuery

--- js/script.js
+++ js/script.js
@@ -104,5 +104,24 @@ $(function() {
 
 
   // 16. Coding Activity: Creating a Signup Lightbox!
-  $(".lightbox").delay(500).fadeIn(1000)
+  // $(".lightbox").delay(500).fadeIn(1000)
+
+
+
+  // Section 3: Element Selectors
+
+  // 17. Using CSS Selectors In jQuery
+  // $("p").css("background-color", "rgba(180, 180, 30, 0.8)")
+  // $(".red-box").css("background-color", "rgba(180, 180, 30, 0.8)")
+  // $("#list").css("background-color", "rgba(180, 180, 30, 0.8)")
+  // $("input[type='text']").css("background-color", "rgba(180, 180, 30, 0.8)")
+  // $("h2, p, input").css("background-color", "rgba(180, 180, 30, 0.8)")
+  // $("p:first").css("background-color", "rgba(180, 180, 30, 0.8)")
+  // $("li:last").css("background-color", "rgba(180, 180, 30, 0.8)")
+  // $("li:even").css("background-color", "rgba(180, 180, 30, 0.8)")
+  // $("li:odd").css("background-color", "rgba(180, 180, 30, 0.8)")
+
+  $("input:text").css("background-color", "rgba(180, 180, 30, 0.8)")
+  // Doesn't exist for "input:email"
+  // Can do for "password", "checkbox", "radio", as well as "checked" and "selected" from <select> tags.
 });
