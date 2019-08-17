e9b20d8e20aa5fb177288884f4cfff75f27e2f8d
eric-klimowich <eklimowich@hotmail.com>
2019-08-17 14:04:25 -0400

    Used focus and blur events

--- js/script.js
+++ js/script.js
@@ -491,12 +491,35 @@ $(function() {
   //   console.log(event.which)
   // })
 
-  var ARROW_RIGHT = 39
-  $("html").keydown(function(event) {
-    if (event.which === ARROW_RIGHT) {
-      $(".blue-box").stop().animate({
-        marginLeft: "+=10px"
-      }, 50)
+  // var ARROW_RIGHT = 39
+  // $("html").keydown(function(event) {
+  //   if (event.which === ARROW_RIGHT) {
+  //     $(".blue-box").stop().animate({
+  //       marginLeft: "+=10px"
+  //     }, 50)
+  //   }
+  // })
+
+
+
+  // Section 7: Events II ‒ Forms
+
+  // 40. The Focus & Blur Events
+  var inputFields = $("input:text, input:password, textarea")
+  inputFields.focus(function() {
+    $(this).css("box-shadow", "0 0 4px gray")
+  })
+
+  inputFields.blur(function() {
+    $(this).css("box-shadow", "none")
+  })
+
+  $("#name").blur(function() {
+    var text = $(this).val()
+    if (text.length < 3) {
+      $(this).css("box-shadow", "0 0 4px red")
+    } else {
+      $(this).css("box-shadow", "0 0 4px green")
     }
   })
 });
