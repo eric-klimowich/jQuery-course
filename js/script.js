9ab80c7b93166b31d0a517eb3d2c7de328982bec
eric-klimowich <eklimowich@hotmail.com>
2019-08-17 20:08:09 -0400

    Rendered additional data, created link on pokemon names, performed another fetch, rendered additional data, including sprites

--- js/script.js
+++ js/script.js
@@ -782,14 +782,49 @@ $(function() {
 
   // 48. Retrieving Pokémon Data from The PokéAPI (Or Star Wars)
   // $.getJSON()
+  // var pokeapiUrl = "https://pokeapi.co/api/v2/generation/1"
+  //
+  // $.getJSON(pokeapiUrl).done(function(data) {
+  //   console.log(data)
+  //   $.each(data.pokemon_species, function(index, pokemon) {
+  //     var name = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)
+  //     var par = $("<p>").html(`Pokemon species no. ${index + 1} is ${name}.`)
+  //     par.appendTo("#pokemon")
+  //   })
+  // }).fail(function() {
+  //   console.log("Request to Pokeapi failed.")
+  // }).always(function() {
+  //   console.log("Pokemon is awesome!")
+  // })
+
+
+  // 49. Improving the Pokédex
   var pokeapiUrl = "https://pokeapi.co/api/v2/generation/1"
+  var pokemonByName = "https://pokeapi.co/api/v2/pokemon/"
 
   $.getJSON(pokeapiUrl).done(function(data) {
     console.log(data)
     $.each(data.pokemon_species, function(index, pokemon) {
       var name = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)
-      var par = $("<p>").html(`Pokemon species no. ${index + 1} is ${name}.`)
+      var link = $("<a>").attr("id", pokemon.name).attr("href", "#").append($("<strong>").text(name))
+      var par = $("<p>").html(`Pokemon species no. ${index + 1} is `).append(link)
+
+      link.click(function(event) {
+        event.preventDefault()
+        $.getJSON(pokemonByName + pokemon.name).done(function(details) {
+          console.log(details)
+          var pokemonDiv = $("#pokemon-details")
+          pokemonDiv.empty()
+          pokemonDiv.append("<h2>" + name + "</h2")
+          pokemonDiv.append(`<img src='${details.sprites.front_default}'>`)
+          pokemonDiv.append(`<img src='${details.sprites.back_default}'>`)
+          pokemonDiv.append(`<img src='${details.sprites.front_shiny}'>`)
+          pokemonDiv.append(`<img src='${details.sprites.back_shiny}'>`)
+        })
+      })
+
       par.appendTo("#pokemon")
+
     })
   }).fail(function() {
     console.log("Request to Pokeapi failed.")
